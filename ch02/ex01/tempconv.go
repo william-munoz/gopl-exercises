9299666b95636471688fe31b59d8e97f8dec82fc
William Munoz <william.munozr@gmail.com>
2021-06-06 16:36:00 -0500

    Add the exercise requirement to the code

--- ch02/ex01/tempconv.go
+++ ch02/ex01/tempconv.go
@@ -1,3 +1,6 @@
+// Exercise 2.1: Add types, constants, and functions to tempconv for processing temperatures in
+// Kelvin scale, where zero Kelvin is -273.15°C and a difference of 1K has the same magnitude as 1°C.
+
 // Package tempconv performs temperature calculations in Celsius, Fahrenheit, and absolute temperatures.
 package tempconv
 
--- ch02/ex03/popcount.go
+++ ch02/ex03/popcount.go
@@ -1,3 +1,7 @@
+// Exercise 2.3: Rewrite PopCount to use a loop instead of a single expression. Compare the performance
+// of the two versions. (Section 11.4 shows how to compare the performance of different implementations
+// systematically.)
+
 // Package popcount returns the population count.
 package popcount
 
